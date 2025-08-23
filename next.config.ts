29385aec775fc71b2efdd44210d8342ef6868803
Idris Ikhennichen <ikhennichen@gmail.com>
2025-08-23 19:27:57 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    // This is required to allow the Next.js dev server to be proxied by the Studio UI
+    // https://nextjs.org/docs/app/api-reference/next-config-js/serverActions#allowedorigins
+    // https://nextjs.org/docs/app/api-reference/components/image#remotepatterns
+    allowedRevalidateHosts: ['*.cloudworkstations.dev'],
+    // This is required to allow the Next.js dev server to be proxied by the Studio UI
+    // https://nextjs.org/docs/app/api-reference/next-config-js/rewrites
+    rewrites: async () => [
+      {
+        // Route all of /__* to the Studio UI on port 6000
+        source: '/__/:path*',
+        destination: 'http://localhost:6000/__/:path*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
